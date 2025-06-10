8f42d0d69869b8e6685ecb15d26d671fbd3af93c
Herni <hernimaryani_2428250017@mhs.mdp.ac.id>
2025-06-10 22:55:34 +0700

    b

---
+++ routes/web copy.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\SesiController;
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/fakultas', FakultasController::class);
+Route::resource('/prodi', ProdiController::class);
+Route::resource('/mahasiswa', MahasiswaController::class);
+Route::resource('/sesi', SesiController::class);
+Route::resource('/mataKuliah', MataKuliahController::class);
+Route::resource('/dosen', DosenController::class);
+Route::resource('/jadwal', JadwalController::class);
+Route::get('/dashboard', [DashboardController::class, 'index']);
