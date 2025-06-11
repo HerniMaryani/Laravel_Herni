916abc3b2aa7c3fd963fe7c8fa173511c122e822
Herni <hernimaryani_2428250017@mhs.mdp.ac.id>
2025-06-11 10:08:58 +0700

    intasll breeze

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,22 @@ use App\Http\Controllers\MataKuliahController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\SesiController;
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::resource('/fakultas', FakultasController::class);
@@ -24,3 +35,5 @@ Route::resource('/mataKuliah', MataKuliahController::class);
 Route::resource('/dosen', DosenController::class);
 Route::resource('/jadwal', JadwalController::class);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+require __DIR__.'/auth.php';
