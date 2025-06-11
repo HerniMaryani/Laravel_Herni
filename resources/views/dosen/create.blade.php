75342dec6d7f7b90e284a107267cdd80da084cfe
Herni <hernimaryani_2428250017@mhs.mdp.ac.id>
2025-06-11 23:08:03 +0700

    Update create.blade.php

--- resources/views/dosen/create.blade.php
+++ resources/views/dosen/create.blade.php
@@ -34,9 +34,12 @@
                         <label for="prodi_id" class="form-label">Program Studi</label>
                         <select name="prodi_id" class="form-control">
                           @foreach ($prodi as $item)
-                            <option value="{{ $item->id }}"> {{ $item->nama }} </option>
+                            <option value="{{ $item->id }}" {{ old('prodi_id') == $item->id ? "selected" : null }}> {{ $item->nama }} </option>
                           @endforeach
                         </select>
+                        @error('prodi_id')
+                          <div class="text-danger">{{ $message }}</div>
+                        @enderror
                       </div>
                     </div>
                     <!--end::Body-->
